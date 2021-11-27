578b9de18d36bba3182ce9f5683a0d7c64348465
Abhinay Thakur <a8h1nay@gmail.com>
2021-11-27 19:23:41 +0530

    Integrate sample charts

--- js/d3-dendrogram.js
+++ js/d3-dendrogram.js
@@ -1,72 +1,83 @@
-export const plotDendrogram = async () => {
-    async => {
-        let svg = d3.select("svg"),
-            width = +svg.attr("width"),
-            height = +svg.attr("height"),
-            g = svg.append("g").attr("transform", "translate(40,0)");
+export const plotDendrogram = () => {
+    //D3
+    let svg = d3.select("#plot-questions")
+        .append("svg")
+        .attr("width", 800)
+        .attr("heigth", 800)
+        .append("g")
+        .attr("transform", "translate(50, 50)");
 
-        let tree = d3.tree()
-            .size([height - 400, width - 160]);
+    let data = [
+        { "child": "John", "parent": "" },
+        { "child": "Aaron", "parent": "John" },
+        { "child": "Kevin", "parent": "Kevin" },
+        { "child": "Hannah", "parent": "Ann" },
+        { "child": "Rose", "parent": "Sarah" },
+        { "child": "Ann", "parent": "John" },
+        { "child": "Sarah", "parent": "Kevin" },
+        { "child": "Mark", "parent": "Ann" },
+        { "child": "Angel", "parent": "Sarah" }
+    ];
 
-        let cluster = d3.cluster()
-            .size([height, width - 160]);
+    // let dataStructure = d3.stratify()
+    //     .id(d => d.child)
+    //     .parentId(d => d.parent)
+    //     (data);
 
-        let stratify = d3.stratify()
-            .parentId(function (d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
+    let chaos = d3.stratify()([
+        { id: "Chaos" },
+        { id: "Gaia", parentId: "Chaos" },
+        { id: "Eros", parentId: "Chaos" },
+        { id: "Erebus", parentId: "Chaos" },
+        { id: "Tartarus", parentId: "Chaos" },
+        { id: "Mountains", parentId: "Gaia" },
+        { id: "Pontus", parentId: "Gaia" },
+        { id: "Uranus", parentId: "Gaia" }
+    ]);
 
-        d3.csv("flare.csv", function (error, data) {
-            if (error) throw error;
-
-            let root = stratify(data)
-                .sort(function (a, b) { return (a.height - b.height) || a.id.localeCompare(b.id); });
-
-            cluster(root);
-
-            let link = g.selectAll(".link")
-                .data(root.descendants().slice(1))
-                .enter().append("path")
-                .attr("class", "link")
-                .attr("d", diagonal);
-
-            let node = g.selectAll(".node")
-                .data(root.descendants())
-                .enter().append("g")
-                .attr("class", function (d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-                .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; });
-
-            node.append("circle")
-                .attr("r", 2.5);
-
-            node.append("text")
-                .attr("dy", 3)
-                .attr("x", function (d) { return d.children ? -8 : 8; })
-                .style("text-anchor", function (d) { return d.children ? "end" : "start"; })
-                .text(function (d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
-
-            d3.selectAll("input")
-                .on("change", changed);
-
-            let timeout = setTimeout(function () {
-                d3.select("input[value=\"tree\"]")
-                    .property("checked", true)
-                    .dispatch("change");
-            }, 1000);
-
-            function changed() {
-                timeout = clearTimeout(timeout);
-                (this.value === "tree" ? tree : cluster)(root);
-                let t = d3.transition().duration(750);
-                node.transition(t).attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; });
-                link.transition(t).attr("d", diagonal);
+    let parsed = d3.hierarchy(
+        "This is not proper parsing. But certainly fun!",
+        id => {
+            for (const split of [/[^\w\s]/, /\s/]) {
+                const children = id && id.split(split).filter(id => id.length > 0);
+                if (children.length > 1) return children;
             }
+        }
+    );
+
+    let dataStructure = d3.hierarchy(data, function (d) { return d.children });
+
+    console.log(dataStructure)
+
+    let treeStructure = d3.tree().size([600, 600]);
+    let information = treeStructure(parsed);
+
+    console.log(information);
+
+    let circles = svg.append("g").selectAll("circle")
+        .data(information.descendants());
+    circles.enter().append("circle")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("r", 5);
+
+    let connections = svg.append("g").selectAll("path")
+        .data(information.links());
+
+    connections.enter()
+        .append("path")
+        .attr("d", d => {
+            return "M" + d.source.x + "," + d.source.y + " C " +
+                d.source.x + "," + (d.source.y + d.target.y) / 2 + " " +
+                d.target.x + "," + (d.source.y + d.target.y) / 2 + " " +
+                d.source.x + "," + d.target.y;
         });
 
-        function diagonal(d) {
-            return "M" + d.y + "," + d.x
-                + "C" + (d.parent.y + 100) + "," + d.x
-                + " " + (d.parent.y + 100) + "," + d.parent.x
-                + " " + d.parent.y + "," + d.parent.x;
-        }
-    }
+    let names = svg.append("g").selectAll("text")
+        .data(information.descendants());
 
+    names.enter().append("text")
+        .text(d => d.data)
+        .attr("x", d => d.x + 5)
+        .attr("y", d => d.y + 2);
 };
--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-import { listenScroll } from './intersection.js';
-// import { plotDendrogram } from './d3-dendrogram.js';
+import { applyScrollSnap } from './intersection.js';
+import { plotDendrogram } from './d3-dendrogram.js';
+import { plotAreaChart } from './d3-areachart.js';
+import { plotLinearScale } from './d3-linearscale.js';
+
 let scene = {
     beforeSearch: null,
     afterSearch: null
@@ -7,37 +10,21 @@ let scene = {
 
 let input;
 
-// let MY_SELECTOR = "#plot-questions" // Could be any selector
-// let observer = new MutationObserver(function (mutations) {
-//     for (let i = 0; i < mutations.length; i++) {
-//         for (let j = 0; j < mutations[i].addedNodes.length; j++) {
-//             // We're iterating through _all_ the elements as the parser parses them,
-//             // deciding if they're the one we're looking for.
-//             if (mutations[i].addedNodes[j].matches(MY_SELECTOR)) {
-
-//                 // We found our element, we're done:
-//                 observer.disconnect();
-//             };
-//         }
-//     }
-// });
-
-// observer.observe(document.documentElement, {
-//     childList: true,
-//     subtree: true
-// });
-
 let appendTemplate = (source, target, isOverwrite) => {
     const sourceEl = document.getElementById(source);
     const targetEl = document.getElementById(target);
-    const clone = sourceEl.content.firstElementChild.cloneNode(true);
+    const clone = sourceEl.content.cloneNode(true);
 
     isOverwrite ? targetEl.innerHTML = null : '';
 
     // Append template clone to target container in DOM
     targetEl.appendChild(clone);
 
-    return targetEl
+    return targetEl;
+}
+
+export const randomIntFromInterval = (min, max) => { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 scene.beforeSearch = appendTemplate("scene-before-search", "app", true);
@@ -47,16 +34,13 @@ scene.beforeSearch.querySelector("#search-btn")
         e.preventDefault();
 
         input = document.getElementById("search-input");
-        scene.afterSearch = appendTemplate("scene-after-search", "app", true);
-        scene.afterSearch
-            .querySelector("#query-value")
-            .innerHTML = input.value;
-
-        listenScroll();
-
-        // Test code (L27 - L40). Refactor it heavily
 
         let sceneLoaded = new Promise((resolve, reject) => {
+            scene.afterSearch = appendTemplate("scene-after-search", "app", true);
+            return !!scene.afterSearch ? resolve("Promise Resolved: After Scene appended") : reject();
+        });
+
+        let sceneTransitioned = new Promise((resolve, reject) => {
             const animatedEl = scene.afterSearch.querySelectorAll(".animate__animated");
 
             animatedEl[1].classList.add('animate__delay-2s');
@@ -71,99 +55,19 @@ scene.beforeSearch.querySelector("#search-btn")
             animatedEl[3].classList.add('animate__fadeInUp');
             animatedEl[4].classList.add('animate__fadeInRight');
 
-            return resolve();
+            return resolve("Promise Resolved: Page transitions added");
         });
 
         sceneLoaded.then(response => {
-            //D3
-            //some async function
-            // console.log(d3);
-            // let url = "university.json";
-            // d3.json(url).then(data => {
-            //     console.log(data);
-            //     let root = d3.hierarchy(data, function (d) {return d.children});
-            //     console.log(x);
-            // });
+            console.log(response);
+            scene.afterSearch.querySelector("#query-value").innerHTML = input.value;
+            applyScrollSnap();
+        });
 
-            let svg = d3.select("#plot-questions")
-                .append("svg")
-                .attr("width", 800)
-                .attr("heigth", 800)
-                .append("g")
-                .attr("transform", "translate(50, 50)");
-
-            let data = [
-                { "child": "John", "parent": "" },
-                { "child": "Aaron", "parent": "John" },
-                { "child": "Kevin", "parent": "Kevin" },
-                { "child": "Hannah", "parent": "Ann" },
-                { "child": "Rose", "parent": "Sarah" },
-                { "child": "Ann", "parent": "John" },
-                { "child": "Sarah", "parent": "Kevin" },
-                { "child": "Mark", "parent": "Ann" },
-                { "child": "Angel", "parent": "Sarah" }
-            ];
-
-            // let dataStructure = d3.stratify()
-            //     .id(d => d.child)
-            //     .parentId(d => d.parent)
-            //     (data);
-
-            let chaos = d3.stratify()([
-                { id: "Chaos" },
-                { id: "Gaia", parentId: "Chaos" },
-                { id: "Eros", parentId: "Chaos" },
-                { id: "Erebus", parentId: "Chaos" },
-                { id: "Tartarus", parentId: "Chaos" },
-                { id: "Mountains", parentId: "Gaia" },
-                { id: "Pontus", parentId: "Gaia" },
-                { id: "Uranus", parentId: "Gaia" }
-            ]);
-
-            let parsed = d3.hierarchy(
-                "This is not proper parsing. But certainly fun!",
-                id => {
-                    for (const split of [/[^\w\s]/, /\s/]) {
-                        const children = id && id.split(split).filter(id => id.length > 0);
-                        if (children.length > 1) return children;
-                    }
-                }
-            );
-
-            let dataStructure = d3.hierarchy(data, function (d) { return d.children });
-
-            console.log(dataStructure)
-
-            let treeStructure = d3.tree().size([600, 600]);
-            let information = treeStructure(parsed);
-
-            console.log(information);
-
-            let circles = svg.append("g").selectAll("circle")
-                .data(information.descendants());
-            circles.enter().append("circle")
-                .attr("cx", d => d.x)
-                .attr("cy", d => d.y)
-                .attr("r", 5);
-
-            let connections = svg.append("g").selectAll("path")
-                .data(information.links());
-
-            connections.enter()
-                .append("path")
-                .attr("d", d => {
-                    return "M" + d.source.x + "," + d.source.y + " C " +
-                        d.source.x + "," + (d.source.y + d.target.y) / 2 + " " +
-                        d.target.x + "," + (d.source.y + d.target.y) / 2 + " " +
-                        d.source.x + "," + d.target.y;
-                });
-
-            let names = svg.append("g").selectAll("text")
-                .data(information.descendants());
-
-            names.enter().append("text")
-                .text(d => d.data.child)
-                .attr("x", d => d.x + 5)
-                .attr("y", d => d.y + 2);
+        sceneTransitioned.then(response => {
+            console.log(response);
+            plotAreaChart();
+            plotLinearScale();
+            plotDendrogram();
         });
     });
