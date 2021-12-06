ee9bfbd4825ae257f88800dd30f75b285abafaa3
Abhinay Thakur <a8h1nay@gmail.com>
2021-12-06 20:07:57 +0530

    Maps navigation step to visible section

--- js/intersection.js
+++ js/intersection.js
@@ -1,26 +1,37 @@
 //The Intersection Observer API
 export const applyScrollSnap = () => {
-    const sections = [...document.querySelectorAll('.graph')]
+    const sections = [...document.querySelectorAll('.graph')];
 
     const options = {
         rootMargin: '0px',
         threshold: 0.25
-    }
+    };
+
+    const nav = document.getElementById("navbar");
+
+    const updateNav = el => {
+        const anchor =  nav.querySelector(`[href="#${el.id}"]`);
+        
+        nav.querySelectorAll("li").forEach(li => li.classList.remove("active"));
+        anchor.parentNode.classList.add("active");
+    };
 
     const callback = (entries) => {
-        entries.forEach((entry) => {
+        entries.forEach(entry => {
             const { target } = entry;
+
             if (entry.intersectionRatio >= options.threshold) {
                 target.classList.add("graph-visible");
+                updateNav(target);
             } else {
                 target.classList.remove("graph-visible");
             }
-        })
-    }
+        });
+    };
 
-    const observer = new IntersectionObserver(callback, options)
+    const observer = new IntersectionObserver(callback, options);
 
     sections.forEach((section, index) => {
-        observer.observe(section)
-    })
+        observer.observe(section);
+    });
 };
