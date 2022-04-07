e73a41c5f3a95752d91b02a7734412debb4704fb
Abhinay Thakur <a8h1nay@gmail.com>
2022-04-07 17:59:46 +0530

    Fixes styling and transition of metric cards

--- src/components/RelatedMetric/RelatedMetric.jsx
+++ src/components/RelatedMetric/RelatedMetric.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./RelatedMetric.sass";
 
-const RelatedMetric = ({ index, chart, metricName }) => {
+const RelatedMetric = ({ index, chart, metricName, metricValue }) => {
     // STATES
 
 
@@ -13,12 +13,26 @@ const RelatedMetric = ({ index, chart, metricName }) => {
 
     return (
         <>
-            <div className="related-metric">
-                <div className="related-metric__num">0{index}</div>
-                <div className="related-metric__chart">
-                    <img src={chart} alt="Chart" />
+            <div className="related-metric-wrapper">
+                <div className="related-metric">
+                    <div className="related-metric__name weight-bold h4">{metricName}</div>
+                    <div className="related-metric__chart">
+                        <img src={chart} alt="Chart" className="chart-img" />
+                    </div>
+                    <div className="related-metric__value weight-bold h2">
+                        <span>{metricValue}</span>
+                    </div>
                 </div>
-                <div className="related-metric__name">{metricName}</div>
+
+                <div className="related-metric-detail">
+                    <div className="related-metric__num weight-bold h4">{++index}</div>
+                    <div className="related-metric__content">
+                        <p className="content-heading weight-bold h4">{metricName}</p>
+                        <p className="content-text weight-regular font-small">
+                            To succeed in the future, organizations must abandon pre-pandemic thinking.
+                        </p>
+                    </div>
+                </div>
             </div>
         </>
     );
